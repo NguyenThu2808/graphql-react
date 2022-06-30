0717412f50df0c550dcc139a3ddb686524f2d382
ThuNT <thunt@beetsoft.com.vn>
2022-06-30 17:12:42 +0700

    init project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HttpLink } from '@apollo/client';
 
+const customFetch = (uri, options) => {
+    let Hawk;
+    const { header } = Hawk.client.header(
+        "http://laravel8_graphql.test/graphql",
+        "POST",
+        { credentials: this.credentials, ext: "some-app-data" }
+    );
+    options.headers.Authorization = header;
+    return fetch(uri, options);
+};
+
+const link = new HttpLink({ fetch: customFetch });
+
+const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache()
+});
+
+// const client = new ApolloClient({
+//     uri: 'http://laravel8_graphql.test/graphql',
+//     cache: new InMemoryCache()
+// })
+
+// Supported in React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+);
